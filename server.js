6e27b0ec50d1cd45c7cbc1640e56e82b9e3bf692
bektafa <bektafa@users.noreply.github.com>
2021-05-19 00:36:08 +0200

    initial commit

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/walter'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/index.html'));});
+'/dist/WALTER/index.html'));});
 app.listen(process.env.PORT || 8080);
