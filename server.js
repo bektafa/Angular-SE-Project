2877c185a7e64dbca5890d1e54ada6176b3e25be
bektafa <bektafa@users.noreply.github.com>
2021-05-19 00:17:39 +0200

    path index.html change

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/walter'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/src/app/meni/meni.component.html/employees'));});
+'/dist/index.html'));});
 app.listen(process.env.PORT || 8080);
